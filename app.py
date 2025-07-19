49db3b800100a69b97964c6c12327f5b56bd3ccd
Chibuzor Agubulom <chibuzor@Chibuzors-MacBook-Pro.local>
2025-07-19 18:41:46 -0500

    edits to app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import OpenAI
@@ -34,22 +36,37 @@ document_search = PineconeVectorStore(
     embedding=embeddingModel
 )
 
-retriever = document_search.as_retriever(search_kwargs={"k":5})
+retriever = document_search.as_retriever(
+    search_type="similarity_score_threshold",
+    search_kwargs={"k":5, "score_threshold": 0.4}
+)
 
-prompt_template = ChatPromptTemplate.from_messages(
+prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
         ("human", "{question}"),
     ]
 )
 
-# question_answer_chain = (
-#     {
-#         "question": itemgetter("question") | "context": retriever.invoke(itemgetter("question"))
-#     }
-#     | prompt 
-#     | model
-# )
+question_answer_chain = (
+    {
+        "context": itemgetter("context"), 
+        "question": itemgetter("question")
+    }
+    | prompt 
+    | model
+    | StrOutputParser()
+)
+
+question_answer_chain = (
+    {
+        "context": retriever, 
+        "question": RunnablePassthrough()
+    }
+    | prompt 
+    | model
+    | StrOutputParser()
+)
 
 @app.route("/")
 def index():
@@ -60,19 +77,21 @@ def chat():
     msg = request.form["msg"]
     logger.info(f"Received message: {msg}")
     
-    prompt = prompt_template.invoke({"question": msg, "context": retriever.invoke(msg)})
-    logger.info(f"Input variables: {prompt}")
+    # input_variables = {"question": msg, "context": retriever.invoke(msg)}
+    # print(f"context: {input_variables['context']}")
     
-    response = model.invoke(prompt)
+    # response = question_answer_chain.invoke(input_variables)
+    response = question_answer_chain.invoke(msg)
+    
     logger.info(f"Response: {response}")
-    response_split = response.strip().split('\n')
-    for line in response_split:
-        if line.startswith('System:'):
-            logger.info(f"System Response: {line.replace('System:', '')}")
-            return line.replace('System:', '')
-        elif line.startswith('answer:'):
-            logger.info(f"System Response: {line.replace('answer:', '')}")
-            return line.replace('System:', '')
+    # response_split = response.strip().split('\n')
+    # for line in response_split:
+    #     if line.startswith('System:'):
+    #         logger.info(f"System Response: {line.replace('System:', '')}")
+    #         return line.replace('System:', '')
+    #     elif line.startswith('answer:'):
+    #         logger.info(f"System Response: {line.replace('answer:', '')}")
+    #         return line.replace('System:', '')
     return response
 
 
